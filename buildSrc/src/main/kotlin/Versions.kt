6229263eb01516e1ed609c4821e1682d91641d99
Stelios Elaiotrivaris <steleot@hotmail.com>
2021-03-13 19:27:01 +0200

    update libraries

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,28 +1,28 @@
 object Versions {
 
     /* kotlin */
-    const val kotlin = "1.4.21-2"
+    const val kotlin = "1.4.31"
     const val coroutines = "1.4.2"
 
     /* plugins */
     const val androidPlugin = "4.1.2"
-    const val benManes = "0.36.0"
-    const val detekt = "1.15.0"
+    const val benManes = "0.38.0"
+    const val detekt = "1.16.0"
     const val jetbrainsDokka = "1.4.20"
     const val jFrogBintray = "1.8.5"
 
     /* libraries */
-    const val activity = "1.2.0-beta02"
-    const val fragment = "1.3.0-beta02"
+    const val activity = "1.2.1"
+    const val fragment = "1.3.1"
     const val core = "1.3.2"
     const val appCompat = "1.2.0"
-    const val lifecycle = "2.2.0"
-    const val material = "1.2.1"
+    const val lifecycle = "2.3.0"
+    const val material = "1.3.0"
     const val recyclerView = "1.1.0"
     const val timber = "4.7.1"
     const val bundler = "1.0.3"
 
     /* test libraries */
-    const val junit = "4.13.1"
+    const val junit = "4.13.2"
     const val androidJunit = "1.1.2"
 }
